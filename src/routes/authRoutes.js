b6348ec975a30d598b6fee95cda9e9e21eff3137
Nsairun <emmansairun@gmail.com>
2026-06-17 13:48:26 +0100

    fixing merge issue

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", (res, req) => {
-    res.json({ message: "auth route working" })
-});
-
-export default router;
--- src/utils/asyncHandler.js
+++
@@ -1,4 +0,0 @@
-const asyncHandler = (fn) => (res, req, next) =>
-    Promise.resolve(fn(req, res, next)).catch(next)
-
-export default asyncHandler;
